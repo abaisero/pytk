e9dcdbef74c9b26abaeb09f0c9144bf91fb1eca5
bigblindbais <andrea.baisero@gmail.com>
2015-11-26 20:33:05 +0100

    saccent -> sentinel

--- pytk/decorators.py
+++ pytk/decorators.py
@@ -19,15 +19,37 @@ def cache(f):
     return wrapper
 
 class lazyprop(property):
+    def __init__(self, fget, doc=None):
+        super(lazyprop, self).__init__(fget=fget, doc=doc)
+        self.pname = '__lazy__{}'.format(fget.__name__)
+
     def __get__(self, obj, objtype=None):
         if obj is None:
             return self
         if self.fget is None:
             raise AttributeError('unreadable attribute')
-        pname = '__lazy__{}'.format(self.fget.__name__)
-        if getattr(obj, pname, None) is None:
-            setattr(obj, pname, self.fget(obj))
-        return getattr(obj, pname)
+        if not hasattr(obj, self.pname):
+            setattr(obj, self.pname, self.fget(obj))
+        return getattr(obj, self.pname)
+
+    def __set__(self, obj, value):
+        if self.fget is None:
+            raise AttributeError('unreadable attribute')
+        setattr(obj, self.pname, value)
+
+    def __del__(self, obj):
+        if self.fget is None:
+            raise AttributeError('unreadable attribute')
+        delattr(obj, self.pname)
+
+    def getter(self, fget):
+        return type(self)(fget, self.__doc__)
+
+    def setter(self, fset):
+        raise AttributeError('Lazyprop does not allow to set explicit setter')
+
+    def deleter(self, fdel):
+        raise AttributeError('Lazyprop does not allow to set explicit deleter')
 
 def static(**kwargs):
     """ static function variables """
@@ -66,4 +88,29 @@ class every(object):
                 self.last = now
                 f(*args, **kwargs)
         return wrapper
+
+class _sentinel_property(property):
+    def __init__(self, fget, watch):
+        super(_sentinel_property, self).__init__(fget=fget)
+        self.watch = watch
+        self.watch_cache = '__sentinel_watch_{}'.format(fget.__name__)
+        self.watchman_cache = '__sentinel_watchman_{}'.format(fget.__name__)
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self.fget is None:
+            raise AttributeError('unreadable attribute')
+        watch = getattr(obj, self.watch)
+        if not hasattr(obj, self.watchman_cache) or getattr(obj, self.watch_cache) is not watch:
+            setattr(obj, self.watch_cache, watch)
+            setattr(obj, self.watchman_cache, self.fget(obj))
+        return getattr(obj, self.watchman_cache)
+
+class sentinel(object):
+    def __init__(self, pname):
+        self.pname = pname
+
+    def __call__(self, f):
+        return _sentinel_property(f, self.pname)
 
