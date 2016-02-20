1ff886137953af28ed26339f650359525dfb0a4d
bigblindbais <andrea.baisero@gmail.com>
2016-02-20 01:39:00 +0100

    typos

--- tests/test_serializable.py
+++ tests/test_serializable.py
@@ -3,14 +3,14 @@ import unittest
 import pytk.pack as pack
 
 
-class CustomClass_Generic(pack.Serializable):
+class Foo_Generic(pack.Serializable):
     def __init__(self, **kwargs):
         self.contents = kwargs
 
     def __str__(self):
         """ Only for debugging the test """
         contents_str = ['{}={}'.format(k, v) for k, v in self.contents.iteritems()]
-        return '(CustomClass_Generic ' + ', '.join(contents_str) + ')'
+        return '(Foo_Generic ' + ', '.join(contents_str) + ')'
 
     def __eq__(self, other):
         """ Only required for testing """
@@ -26,13 +26,13 @@ class CustomClass_Generic(pack.Serializable):
         return cls(**data)
 
 
-class CustomClass_Special(pack.Serializable):
+class Foo_Special(pack.Serializable):
     def __init__(self, i, j):
         self.i = i
         self.j = j
 
     def __str__(self):
-        return '(CustomClass_Special i={}, j={})'.format(self.i, self.j)
+        return '(Foo_Special i={}, j={})'.format(self.i, self.j)
 
     def __eq__(self, other):
         return self.i == other.i and self.j == other.j
@@ -48,56 +48,58 @@ class CustomClass_Special(pack.Serializable):
         return cls(i, j)
 
 
-class PackTest(unittest.TestCase):
-    def pack_unpack(self, obj):
-        return pack.unpackb(pack.packb(obj))
+def pack_unpack(obj):
+    packed = pack.packb(obj)
+    return pack.unpackb(packed)
 
+
+class PackTest(unittest.TestCase):
     def test_builtin_types(self):
-        """ Builtin types can be unpacked """
+        # Builtin types can be unpacked
         original = 1
-        restored = self.pack_unpack(original)
+        restored = pack_unpack(original)
         self.assertEqual(original, restored)
 
         original = [1, 2, 3]
-        restored = self.pack_unpack(original)
+        restored = pack_unpack(original)
         self.assertEqual(original, restored)
 
         original = dict(a=1, b=2)
-        restored = self.pack_unpack(original)
+        restored = pack_unpack(original)
         self.assertEqual(original, restored)
 
     def test_custom_types(self):
-        """ Serializable objects can be unpacked """
-        original = CustomClass_Generic(i=1, j=2, k=3)
-        restored = self.pack_unpack(original)
+        # Serializable objects can be unpacked
+        original = Foo_Generic(i=1, j=2, k=3)
+        restored = pack_unpack(original)
         self.assertEqual(original, restored)
 
-        original = CustomClass_Special(1, 2)
-        restored = self.pack_unpack(original)
+        original = Foo_Special(1, 2)
+        restored = pack_unpack(original)
         self.assertEqual(original, restored)
 
     def test_mixed_types(self):
-        """ Objects can contain other objects """
-        original = [CustomClass_Generic(i=1, j=2, k=3), CustomClass_Special(4, 5), [6, 7]]
-        restored = self.pack_unpack(original)
+        # Objects can contain other objects
+        original = [Foo_Generic(i=1, j=2, k=3), Foo_Special(4, 5), [6, 7]]
+        restored = pack_unpack(original)
         self.assertEqual(original, restored)
 
-        original = dict(a=CustomClass_Generic(i=1, j=2, k=3), b=CustomClass_Special(4, 5), c=[6, 7])
-        restored = self.pack_unpack(original)
+        original = dict(a=Foo_Generic(i=1, j=2, k=3), b=Foo_Special(4, 5), c=[6, 7])
+        restored = pack_unpack(original)
         self.assertEqual(original, restored)
 
     def test_recursiveness(self):
-        """ Serializable objects can contain other Serializable objects """
-        obj1 = [1, CustomClass_Generic(a=2, b=3), CustomClass_Special(4, 5)]
-        obj2 = CustomClass_Generic(a=obj1, b=CustomClass_Special(6, 7), c=8)
-        obj3 = CustomClass_Special(obj2, CustomClass_Generic(a=obj1, b=9, c=obj2, d=10))
+        # Serializable objects can contain other Serializable objects
+        obj1 = [1, Foo_Generic(a=2, b=3), Foo_Special(4, 5)]
+        obj2 = Foo_Generic(a=obj1, b=Foo_Special(6, 7), c=8)
+        obj3 = Foo_Special(obj2, Foo_Generic(a=obj1, b=9, c=obj2, d=10))
         original = [obj1, obj2, obj3]
-        restored = self.pack_unpack(original)
+        restored = pack_unpack(original)
         self.assertEqual(original, restored)
 
     def test_references(self):
-        """ Packed references to the same object are unpacked as references to the same object """
-        obj = CustomClass_Generic(a=1, b=2, c=3)
+        # Packed references to the same object are unpacked as references to the same object
+        obj = Foo_Generic(a=1, b=2, c=3)
         original = [obj, obj]
-        restored = self.pack_unpack(original)
+        restored = pack_unpack(original)
         self.assertIs(restored[0], restored[1])
