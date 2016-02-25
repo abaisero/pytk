489c36f3c7c2d713ec6b75f5fd5fdaecaa800cb2
bigblindbais <andrea.baisero@gmail.com>
2016-02-25 02:11:10 +0100

    test_util works

--- pytk/util.py
+++ pytk/util.py
@@ -3,12 +3,12 @@
 
 class Hashable(object):
     @property
-    def _key(self):
+    def _hashable_key(self):
         raise Exception
 
     def __hash__(self):
-        return hash(self._key)
-        # return reduce(operator.xor, map(hash, self._key))
+        return hash(self._hashable_key)
+        # return reduce(operator.xor, map(hash, self._hashable_key))
 
     def __eq__(self, other):
-        isinstance(self, type(other)) and self._key == self._key
+        return isinstance(self, type(other)) and self._hashable_key == other._hashable_key
---
+++ tests/test_util.py
@@ -0,0 +1,25 @@
+import unittest2 as unittest
+
+import random
+import pytk.util as util
+
+
+class Foo(util.Hashable):
+    def __init__(self, i):
+        self.i = i
+
+    @util.Hashable._hashable_key.getter
+    def _hashable_key(self):
+        return (self.i,)
+
+
+class UtilTest(unittest.TestCase):
+    def test_hashable(self):
+        n = 10
+        indl = [random.randint(-10, 10) for i in range(n)]
+        inds = set(indl)
+
+        fool = map(Foo, indl)
+        foos = set(fool)
+
+        self.assertEqual(len(foos), len(inds))
