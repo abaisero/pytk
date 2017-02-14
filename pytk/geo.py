59b5d90e9327ec31b2cd19142ea22f841b1ea24c
abaisero <andrea.baisero@gmail.com>
2017-02-14 17:28:32 +0100

    new geo module; 3d vectors, quaternions and rotation quaternions

--- pytk/geo.py
+++ pytk/geo.py
@@ -1,27 +1,188 @@
-Q = namedtuple('Q', ['w', 'x', 'y', 'z'])
+from __future__ import division
 
-# class Q(namedtuple('Q', ['x', 'y', 'z', 'w']):
-#     def normalize(self):
-#         self.q /= la.norm(self.q)
+import numpy as np
+import numpy.linalg as la
 
-#     def rotate(self, p):
 
-class Q(object):
-    def __init__(self, x=0., y=0., z=0., w=1.):
-        self.x
-        self.y
-        self.z
-        self.w
+class GeoException(Exception):
+    pass
+
+
+class vect(object):
+    def __init__(self, xyz):
+        self.xyz = xyz
 
     @property
-    def a(self):
-        return = np.array([self.x, self.y, self.z, self.w])
+    def xyz(self):
+        return self.__xyz
+
+    @xyz.setter
+    def xyz(self, value):
+        value = np.array(value)
+        if value.shape != (3,):
+            raise GeoException('Vector needs 3 values (given {})'.format(value.shape))
+        self.__xyz = value
+
+    @property
+    def as_quat(self):
+        return quat(np.r_[0, self.xyz])
+
+    def __pow__(self, p):
+        return np.dot(self.xyz, self.xyz) ** (p / 2)
+
+    def __len__(self):
+        return self ** 1
+
+    def __neg__(self):
+        return vect(-self.xyz)
+
+    def __add__(self, other):
+        return vect(self.xyz + other.xyz)
+
+    def __sub__(self, other):
+        return vect(self.xyz - other.xyz)
+    
+    def __eq__(self, other):
+        return np.allclose(self.xyz, other.xyz)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __repr__(self):
+        return 'vect({})'.format(str(self.xyz))
+
+    def __str__(self):
+        return 'vect({})'.format(str(self.xyz))
+
+
+class quat(object):
+    __mulmat__ = np.array([
+        [
+            [ 1,  0,  0,  0],
+            [ 0, -1,  0,  0],
+            [ 0,  0, -1,  0],
+            [ 0,  0,  0, -1],
+        ],
+        [
+            [ 0,  1,  0,  0],
+            [ 1,  0,  0,  0],
+            [ 0,  0,  0,  1],
+            [ 0,  0, -1,  0],
+        ],
+        [
+            [ 0,  0,  1,  0],
+            [ 0,  0,  0, -1],
+            [ 1,  0,  0,  0],
+            [ 0,  1,  0,  0],
+        ],
+        [
+            [ 0,  0,  0,  1],
+            [ 0,  0,  1,  0],
+            [ 0, -1,  0,  0],
+            [ 1,  0,  0,  0],
+        ],
+    ])
+
+    def __init__(self, wxyz):
+        self.wxyz = wxyz
+
+    @property
+    def wxyz(self):
+        return self.__wxyz
+
+    @wxyz.setter
+    def wxyz(self, value):
+        value = np.array(value)
+        if value.shape != (4,):
+            raise GeoException('Quaternion needs 4 values (given {})'.format(value.shape))
+        self.__wxyz = value
+
+    @property
+    def w(self):
+        return self.wxyz[0]
+
+    @property
+    def xyz(self):
+        return self.wxyz[1:]
+
+    @property
+    def as_rquat(self):
+        return rquat(self.wxyz)
+
+    @property
+    def as_vect(self):
+        return vect(self.xyz)
+
+    @property
+    def conj(self):
+        return type(self)(self.wxyz * [1, -1, -1, -1])
+
+    @property
+    def inv(self):
+        return type(self)(self.conj.wxyz / self ** 2)
+
+    @property
+    def normal(self):
+        return type(self)(self.wxyz / self ** 1)
+
+    def __pow__(self, p):
+        return np.dot(self.wxyz, self.wxyz) ** (p / 2)
+
+    def __len__(self):
+        return self ** 1
+
+    def __neg__(self):
+        return type(self)(-self.wxyz)
+
+    def __add__(self, other):
+        return quat(self.wxyz + other.wxyz)
+
+    def __sub__(self, other):
+        return quat(self.wxyz - other.wxyz)
 
     def __mul__(self, other):
+        # NOTE returns rquat instance only if both self and other are rquats
+        qclass = type(self) if isinstance(other, rquat) else quat
+        wxyz = np.einsum('kij,i,j->k', quat.__mulmat__, self.wxyz, other.wxyz)
+        return qclass(wxyz)
+
+    def __div__(self, other):
+        return self * other.inv
+
+    def __eq__(self, other):
+        return np.allclose(self.wxyz, other.wxyz)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __repr__(self):
+        return '{}({})'.format(type(self), str(self.wxyz))
+
+    def __str__(self):
+        return '{}({})'.format(type(self), str(self.wxyz))
 
 
+class rquat(quat):
+    @property
+    def wxyz(self):
+        return super(rquat, self).wxyz
 
-if __name__ == '__main__':
-    q = Quaternion()
+    @wxyz.setter
+    def wxyz(self, value):
+        super(rquat, type(self)).wxyz.fset(self, value)
+        self.__wxyz = self.wxyz / self ** 1
 
-    Q(1, 2, 3, 4).normalize()
+    @property
+    def as_rquat(self):
+        return self 
+
+    def __mul__(self, other):
+        try:
+            return super(rquat, self).__mul__(other)
+        except AttributeError:
+            xyz = (self.conj * other.as_quat * self).xyz
+            return vect(xyz)
+
+    def __eq__(self, other):
+        return super(rquat, self).__eq__(other) or \
+                super(rquat, self).__eq__(-other)
---
+++ tests/test_geo.py
@@ -0,0 +1,337 @@
+import unittest2 as unittest
+
+import numpy as np
+import numpy.testing as npt
+
+import pytk.geo as geo
+
+
+class GeoVectTest(unittest.TestCase):
+
+    def test_shape(self):
+        self.assertRaises(geo.GeoException, geo.vect, [])
+        self.assertRaises(geo.GeoException, geo.vect, [0])
+        self.assertRaises(geo.GeoException, geo.vect, [0, 1])
+        self.assertRaises(geo.GeoException, geo.vect, [0, 1, 2, 3])
+        self.assertRaises(geo.GeoException, geo.vect, [0, 1, 2, 3, 4])
+        self.assertRaises(geo.GeoException, geo.vect, [0, 1, 2, 3, 4, 5])
+        self.assertRaises(geo.GeoException, geo.vect, [0, 1, 2, 3, 4, 5, 6])
+        self.assertRaises(geo.GeoException, geo.vect, [0, 1, 2, 3, 4, 5, 6, 7])
+
+    def test_equality(self):
+        x = geo.vect([1, 0, 0])
+        y = geo.vect([0, 1, 0])
+        z = geo.vect([0, 0, 1])
+
+        self.assertEqual(x, x)
+        self.assertEqual(y, y)
+        self.assertEqual(z, z)
+
+        self.assertNotEqual(x, y)
+        self.assertNotEqual(x, z)
+        self.assertNotEqual(y, z)
+
+    def test_add_sub(self):
+        x = geo.vect([1, 0, 0])
+        y = geo.vect([0, 1, 0])
+        z = geo.vect([0, 0, 1])
+
+        self.assertEqual(x + y    , geo.vect([1,  1,  0]))
+        self.assertEqual(x     + z, geo.vect([1,  0,  1]))
+        self.assertEqual(    y + z, geo.vect([0,  1,  1]))
+        self.assertEqual(x - y    , geo.vect([1, -1,  0]))
+        self.assertEqual(x     - z, geo.vect([1,  0, -1]))
+        self.assertEqual(    y - z, geo.vect([0,  1, -1]))
+
+    def test_neg(self):
+        x = geo.vect([1, 0, 0])
+        y = geo.vect([0, 1, 0])
+        z = geo.vect([0, 0, 1])
+
+        self.assertEqual(x - y, x + (-y))
+        self.assertEqual(x - z, x + (-z))
+        self.assertEqual(y - z, y + (-z))
+
+    def test_pow(self):
+        x = geo.vect([1, 0, 0])
+        y = geo.vect([0, 1, 0])
+        z = geo.vect([0, 0, 1])
+
+        self.assertAlmostEqual(x ** 1, 1)
+        self.assertAlmostEqual(y ** 1, 1)
+        self.assertAlmostEqual(z ** 1, 1)
+        self.assertAlmostEqual(x ** 2, 1)
+        self.assertAlmostEqual(y ** 2, 1)
+        self.assertAlmostEqual(z ** 2, 1)
+
+        self.assertAlmostEqual((x + y    ) ** 1, np.sqrt(2))
+        self.assertAlmostEqual((x     + z) ** 1, np.sqrt(2))
+        self.assertAlmostEqual((    y + z) ** 1, np.sqrt(2)) 
+        self.assertAlmostEqual((x - y    ) ** 1, np.sqrt(2)) 
+        self.assertAlmostEqual((x     - z) ** 1, np.sqrt(2)) 
+        self.assertAlmostEqual((    y - z) ** 1, np.sqrt(2)) 
+        self.assertAlmostEqual((x + y    ) ** 2, 2)
+        self.assertAlmostEqual((x     + z) ** 2, 2)
+        self.assertAlmostEqual((    y + z) ** 2, 2) 
+        self.assertAlmostEqual((x - y    ) ** 2, 2) 
+        self.assertAlmostEqual((x     - z) ** 2, 2) 
+        self.assertAlmostEqual((    y - z) ** 2, 2) 
+
+
+class GeoQuatTest(unittest.TestCase):
+
+    def test_shape(self):
+        self.assertRaises(geo.GeoException, geo.quat, [])
+        self.assertRaises(geo.GeoException, geo.quat, [0])
+        self.assertRaises(geo.GeoException, geo.quat, [0, 1])
+        self.assertRaises(geo.GeoException, geo.quat, [0, 1, 2])
+        self.assertRaises(geo.GeoException, geo.quat, [0, 1, 2, 3, 4])
+        self.assertRaises(geo.GeoException, geo.quat, [0, 1, 2, 3, 4, 5])
+        self.assertRaises(geo.GeoException, geo.quat, [0, 1, 2, 3, 4, 5, 6])
+        self.assertRaises(geo.GeoException, geo.quat, [0, 1, 2, 3, 4, 5, 6, 7])
+
+    def test_equality(self):
+        w = geo.quat([1, 0, 0, 0])
+        x = geo.quat([0, 1, 0, 0])
+        y = geo.quat([0, 0, 1, 0])
+        z = geo.quat([0, 0, 0, 1])
+
+        self.assertEqual(w, w)
+        self.assertEqual(x, x)
+        self.assertEqual(y, y)
+        self.assertEqual(z, z)
+
+        self.assertNotEqual(w, x)
+        self.assertNotEqual(w, y)
+        self.assertNotEqual(w, z)
+        self.assertNotEqual(x, y)
+        self.assertNotEqual(x, z)
+        self.assertNotEqual(y, z)
+
+    def test_add_sub(self):
+        w = geo.quat([1, 0, 0, 0])
+        x = geo.quat([0, 1, 0, 0])
+        y = geo.quat([0, 0, 1, 0])
+        z = geo.quat([0, 0, 0, 1])
+
+        self.assertEqual(  w + x + y    , geo.quat([1, 1, 1, 0]))
+        self.assertEqual(  w + x     + z, geo.quat([1, 1, 0, 1]))
+        self.assertEqual(  w +     y + z, geo.quat([1, 0, 1, 1]))
+        self.assertEqual(      x + y + z, geo.quat([0, 1, 1, 1]))
+
+        self.assertEqual(  w - x + y    , geo.quat([ 1, -1,  1,  0]))
+        self.assertEqual(  w - x     + z, geo.quat([ 1, -1,  0,  1]))
+        self.assertEqual(  w     - y + z, geo.quat([ 1,  0, -1,  1]))
+        self.assertEqual(      x - y + z, geo.quat([ 0,  1, -1,  1]))
+        self.assertEqual(- w + x - y    , geo.quat([-1,  1, -1,  0]))
+        self.assertEqual(- w + x     - z, geo.quat([-1,  1,  0, -1]))
+        self.assertEqual(- w     + y - z, geo.quat([-1,  0,  1, -1]))
+        self.assertEqual(    - x + y - z, geo.quat([ 0, -1,  1, -1]))
+
+    def test_neg(self):
+        w = geo.quat([1, 0, 0, 0])
+        x = geo.quat([0, 1, 0, 0])
+        y = geo.quat([0, 0, 1, 0])
+        z = geo.quat([0, 0, 0, 1])
+
+        self.assertEqual(w - x, w + (-x))
+        self.assertEqual(w - y, w + (-y))
+        self.assertEqual(w - z, w + (-z))
+        self.assertEqual(x - y, x + (-y))
+        self.assertEqual(x - z, x + (-z))
+        self.assertEqual(y - z, y + (-z))
+
+    def test_pow(self):
+        w = geo.quat([1, 0, 0, 0])
+        x = geo.quat([0, 1, 0, 0])
+        y = geo.quat([0, 0, 1, 0])
+        z = geo.quat([0, 0, 0, 1])
+
+        self.assertAlmostEqual(w ** 2, 1)
+        self.assertAlmostEqual(x ** 2, 1)
+        self.assertAlmostEqual(y ** 2, 1)
+        self.assertAlmostEqual(z ** 2, 1)
+
+        self.assertAlmostEqual((  w - x + y    ) ** 2, 3)
+        self.assertAlmostEqual((  w - x     + z) ** 2, 3)
+        self.assertAlmostEqual((  w     - y + z) ** 2, 3)
+        self.assertAlmostEqual((      x - y + z) ** 2, 3)
+        self.assertAlmostEqual((- w + x - y    ) ** 2, 3)
+        self.assertAlmostEqual((- w + x     - z) ** 2, 3)
+        self.assertAlmostEqual((- w     + y - z) ** 2, 3)
+        self.assertAlmostEqual((    - x + y - z) ** 2, 3)
+
+    def test_conj(self):
+        w = geo.quat([1, 0, 0, 0])
+        x = geo.quat([0, 1, 0, 0])
+        y = geo.quat([0, 0, 1, 0])
+        z = geo.quat([0, 0, 0, 1])
+
+        self.assertEqual(w + w.conj, geo.quat([2, 0, 0, 0]))
+        self.assertEqual(x + x.conj, geo.quat([0, 0, 0, 0]))
+        self.assertEqual(y + y.conj, geo.quat([0, 0, 0, 0]))
+        self.assertEqual(z + z.conj, geo.quat([0, 0, 0, 0]))
+
+    def test_inv(self):
+        w = geo.quat([1, 0, 0, 0])
+        x = geo.quat([0, 1, 0, 0])
+        y = geo.quat([0, 0, 1, 0])
+        z = geo.quat([0, 0, 0, 1])
+
+        self.assertEqual(w * w.inv, geo.quat([1, 0, 0, 0]))
+        self.assertEqual(w.inv * w, geo.quat([1, 0, 0, 0]))
+        self.assertEqual(x * x.inv, geo.quat([1, 0, 0, 0]))
+        self.assertEqual(x.inv * x, geo.quat([1, 0, 0, 0]))
+        self.assertEqual(y * y.inv, geo.quat([1, 0, 0, 0]))
+        self.assertEqual(y.inv * y, geo.quat([1, 0, 0, 0]))
+        self.assertEqual(z * z.inv, geo.quat([1, 0, 0, 0]))
+        self.assertEqual(z.inv * z, geo.quat([1, 0, 0, 0]))
+
+    def test_normal(self):
+        w = geo.quat([1, 0, 0, 0])
+        x = geo.quat([0, 1, 0, 0])
+        y = geo.quat([0, 0, 1, 0])
+        z = geo.quat([0, 0, 0, 1])
+
+        self.assertAlmostEqual((w            ).normal ** 2, 1) 
+        self.assertAlmostEqual((    x        ).normal ** 2, 1) 
+        self.assertAlmostEqual((        y    ).normal ** 2, 1) 
+        self.assertAlmostEqual((            z).normal ** 2, 1) 
+        self.assertAlmostEqual((w + x        ).normal ** 2, 1) 
+        self.assertAlmostEqual((w     + y    ).normal ** 2, 1) 
+        self.assertAlmostEqual((w         + z).normal ** 2, 1) 
+        self.assertAlmostEqual((    x + y    ).normal ** 2, 1) 
+        self.assertAlmostEqual((    x     + z).normal ** 2, 1) 
+        self.assertAlmostEqual((        y + z).normal ** 2, 1) 
+        self.assertAlmostEqual((w + x + y    ).normal ** 2, 1) 
+        self.assertAlmostEqual((w + x     + z).normal ** 2, 1) 
+        self.assertAlmostEqual((w +     y + z).normal ** 2, 1) 
+        self.assertAlmostEqual((    x + y + z).normal ** 2, 1) 
+        self.assertAlmostEqual((w + x + y + z).normal ** 2, 1) 
+
+    def test_as_rquat(self):
+        w = geo.quat([1, 0, 0, 0])
+        x = geo.quat([0, 1, 0, 0])
+        y = geo.quat([0, 0, 1, 0])
+        z = geo.quat([0, 0, 0, 1])
+
+        self.assertEqual((w            ).as_rquat, rquat([1, 0, 0, 0])) 
+        self.assertEqual((    x        ).as_rquat, rquat([0, 1, 0, 0])) 
+        self.assertEqual((        y    ).as_rquat, rquat([0, 0, 1, 0])) 
+        self.assertEqual((            z).as_rquat, rquat([0, 0, 0, 1])) 
+        self.assertEqual((w + x        ).as_rquat, rquat([1, 1, 0, 0])) 
+        self.assertEqual((w     + y    ).as_rquat, rquat([1, 0, 1, 0])) 
+        self.assertEqual((w         + z).as_rquat, rquat([1, 0, 0, 1])) 
+        self.assertEqual((    x + y    ).as_rquat, rquat([0, 1, 1, 0])) 
+        self.assertEqual((    x     + z).as_rquat, rquat([0, 1, 0, 1])) 
+        self.assertEqual((        y + z).as_rquat, rquat([0, 0, 1, 1])) 
+        self.assertEqual((w + x + y    ).as_rquat, rquat([1, 1, 1, 0])) 
+        self.assertEqual((w + x     + z).as_rquat, rquat([1, 1, 0, 1])) 
+        self.assertEqual((w +     y + z).as_rquat, rquat([1, 0, 1, 1])) 
+        self.assertEqual((    x + y + z).as_rquat, rquat([0, 1, 1, 1])) 
+        self.assertEqual((w + x + y + z).as_rquat, rquat([1, 1, 1, 1])) 
+
+    def test_as_vect(self):
+        w = geo.quat([1, 0, 0, 0])
+        x = geo.quat([0, 1, 0, 0])
+        y = geo.quat([0, 0, 1, 0])
+        z = geo.quat([0, 0, 0, 1])
+
+        self.assertEqual((w            ).as_vect, vect([0, 0, 0])) 
+        self.assertEqual((    x        ).as_vect, vect([1, 0, 0])) 
+        self.assertEqual((        y    ).as_vect, vect([0, 1, 0])) 
+        self.assertEqual((            z).as_vect, vect([0, 0, 1])) 
+        self.assertEqual((w + x        ).as_vect, vect([1, 0, 0])) 
+        self.assertEqual((w     + y    ).as_vect, vect([0, 1, 0])) 
+        self.assertEqual((w         + z).as_vect, vect([0, 0, 1])) 
+        self.assertEqual((    x + y    ).as_vect, vect([1, 1, 0])) 
+        self.assertEqual((    x     + z).as_vect, vect([1, 0, 1])) 
+        self.assertEqual((        y + z).as_vect, vect([0, 1, 1])) 
+        self.assertEqual((w + x + y    ).as_vect, vect([1, 1, 0])) 
+        self.assertEqual((w + x     + z).as_vect, vect([1, 0, 1])) 
+        self.assertEqual((w +     y + z).as_vect, vect([0, 1, 1])) 
+        self.assertEqual((    x + y + z).as_vect, vect([1, 1, 1])) 
+        self.assertEqual((w + x + y + z).as_vect, vect([1, 1, 1])) 
+
+    def test_mul(self):
+        w = geo.quat([1, 0, 0, 0])
+        x = geo.quat([0, 1, 0, 0])
+        y = geo.quat([0, 0, 1, 0])
+        z = geo.quat([0, 0, 0, 1])
+
+        self.assertEqual( w * x         , geo.quat([ 0,  1,  0,  0]))
+        self.assertEqual( w     * y     , geo.quat([ 0,  0,  1,  0]))
+        self.assertEqual( w         * z , geo.quat([ 0,  0,  0,  1]))
+        self.assertEqual(     x * y     , geo.quat([ 0,  0,  0,  1]))
+        self.assertEqual(     x     * z , geo.quat([ 0,  0, -1,  0]))
+        self.assertEqual(         y * z , geo.quat([ 0,  1,  0,  0]))
+        self.assertEqual( w * x * y     , geo.quat([ 0,  0,  0,  1]))
+        self.assertEqual( w * x     * z , geo.quat([ 0,  0, -1,  0]))
+        self.assertEqual( w     * y * z , geo.quat([ 0,  1,  0,  0]))
+        self.assertEqual(     x * y * z , geo.quat([-1,  0,  0,  0]))
+        self.assertEqual( w * x * y * z , geo.quat([-1,  0,  0,  0]))
+
+        self.assertEqual((w * x) * y * z, geo.quat([-1,  0,  0,  0]))
+        self.assertEqual(w * (x * y) * z, geo.quat([-1,  0,  0,  0]))
+        self.assertEqual(w * x * (y * z), geo.quat([-1,  0,  0,  0]))
+
+        self.assertNotEqual(x * y, y * x)
+        self.assertNotEqual(x * z, z * x)
+        self.assertNotEqual(y * z, z * y)
+
+    def test_rotate(self):
+        v = geo.vect([0, 1, 2])
+        w = geo.quat([1, 0, 0, 0])
+        x = geo.quat([0, 1, 0, 0])
+        y = geo.quat([0, 0, 1, 0])
+        z = geo.quat([0, 0, 0, 1])
+
+        self.assertEqual(w             * v, geo.vect([0,  1,  2]))
+        self.assertEqual(    x         * v, geo.vect([0, -1, -2]))
+        self.assertEqual(        y     * v, geo.vect([0,  1, -2]))
+        self.assertEqual(            z * v, geo.vect([0, -1,  2]))
+        self.assertEqual(w * x         * v, geo.vect([0, -1, -2]))
+        self.assertEqual(w     * y     * v, geo.vect([0,  1, -2]))
+        self.assertEqual(w         * z * v, geo.vect([0, -1,  2]))
+        self.assertEqual(    x * y     * v, geo.vect([0, -1,  2]))
+        self.assertEqual(    x     * z * v, geo.vect([0,  1, -2]))
+        self.assertEqual(        y * z * v, geo.vect([0, -1, -2]))
+        self.assertEqual(w * x * y     * v, geo.vect([0, -1,  2]))
+        self.assertEqual(w * x     * z * v, geo.vect([0,  1, -2]))
+        self.assertEqual(w     * y * z * v, geo.vect([0, -1, -2]))
+        self.assertEqual(    x * y * z * v, geo.vect([0,  1,  2]))
+        self.assertEqual(w * x * y * z * v, geo.vect([0,  1,  2]))
+
+        self.assertEqual((w * x) * y * z * v, geo.vect([0, 1, 2]))
+        self.assertEqual(w * (x * y) * z * v, geo.vect([0, 1, 2]))
+        self.assertEqual(w * x * (y * z) * v, geo.vect([0, 1, 2]))
+        self.assertEqual(w * x * y * (z * v), geo.vect([0, 1, 2]))
+
+        self.assertEqual((w * x * y * z) * (v), geo.vect([0,  1,  2]))
+        self.assertEqual((w * x * y) * (z * v), geo.vect([0,  1,  2]))
+        self.assertEqual((w * x) * (y * z * v), geo.vect([0,  1,  2]))
+        self.assertEqual((w) * (x * y * z * v), geo.vect([0,  1,  2]))
+
+
+class GeoQuatTest(unittest.TestCase):
+    def test_pow(self):
+        w = geo.rquat([1, 0, 0, 0])
+        x = geo.rquat([0, 1, 0, 0])
+        y = geo.rquat([0, 0, 1, 0])
+        z = geo.rquat([0, 0, 0, 1])
+
+        self.assertAlmostEqual((w            ) ** 2, 1) 
+        self.assertAlmostEqual((    x        ) ** 2, 1) 
+        self.assertAlmostEqual((        y    ) ** 2, 1) 
+        self.assertAlmostEqual((            z) ** 2, 1) 
+        self.assertAlmostEqual((w * x        ) ** 2, 1) 
+        self.assertAlmostEqual((w     * y    ) ** 2, 1) 
+        self.assertAlmostEqual((w         * z) ** 2, 1) 
+        self.assertAlmostEqual((    x * y    ) ** 2, 1) 
+        self.assertAlmostEqual((    x     * z) ** 2, 1) 
+        self.assertAlmostEqual((        y * z) ** 2, 1) 
+        self.assertAlmostEqual((w * x * y    ) ** 2, 1) 
+        self.assertAlmostEqual((w * x     * z) ** 2, 1) 
+        self.assertAlmostEqual((w *     y * z) ** 2, 1) 
+        self.assertAlmostEqual((    x * y * z) ** 2, 1) 
+        self.assertAlmostEqual((w * x * y * z) ** 2, 1) 
